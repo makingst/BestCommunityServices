5edb87a4ff76d88e7fea5045efa73a95a95ffc09
makingst <makingst@asu.edu>
2021-02-09 21:05:34 -0600

    add nav and home page

---
+++ ser416project/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './pages/Home'
+import Nav from './components/Nav';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Home />
+        
+    </div>
+  );
+}
+
+export default App;
